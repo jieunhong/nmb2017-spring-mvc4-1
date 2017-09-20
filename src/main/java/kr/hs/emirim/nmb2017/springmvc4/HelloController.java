046105c2a6931e449baa1449222f82e48798ac40
jieunhong <hyy0786@naver.com>
2017-09-20 20:12:00 +0900

    스프링 수행평가 프로젝트2

---
+++ src/main/java/kr/hs/emirim/nmb2017/springmvc4/HelloController.java
@@ -0,0 +1,20 @@
+package kr.hs.emirim.nmb2017.springmvc4;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	/*@RequestMapping("/")
+	public String hello() {
+		return "resultPage";
+	}*/
+	
+//	@ResponseBody
+//	public String hello() {
+//		return "Hello, World!";
+//	}
+
+}
